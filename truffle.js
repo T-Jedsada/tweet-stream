86c13a162bfbd38d467a192d5ac0f5fc04ce609f
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-09-15 00:52:02 +0700

    add kovan network for deploy

--- truffle.js
+++ truffle.js
@@ -1,4 +1,9 @@
 var path = require('path');
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = 'width unknown zone beach room derive attack keen swear kangaroo brother final'
+
 module.exports = {
     contracts_build_directory: path.join(__dirname, "src/contracts/"),
     networks: {
@@ -10,10 +15,15 @@ module.exports = {
         gas: 4712388
       },
       rinkeby: {
-        host: "localhost",
-        port: 9545,
+        provider: () => new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/3tb555OeVijXQHNSyKSP"),
         network_id: "4", // rinkeby network ID
-        from: "0x44a0579754d6c94e7bb2c26bfa7394311cc50ccb", // default address to use for any transaction Truffle makes during migrations
+        from: "0x33503f0e83f3d32673344e873b5f80553d7861cd", // default address to use for any transaction Truffle makes during migrations
+        gas: 4612388 // Gas limit used for deploys
+      },
+      koven: {
+        provider: () => new HDWalletProvider(mnemonic, "https://kovan.infura.io/v3/b125d0dc8f8f4f3e9b5350d25a1ce1b0"),
+        network_id: "3", // rinkeby network ID
+        from: "0x33503f0e83f3d32673344e873b5f80553d7861cd", // default address to use for any transaction Truffle makes during migrations
         gas: 4612388 // Gas limit used for deploys
       }
     }
